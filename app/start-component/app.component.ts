7ddc137794eea5012653f8d1af19560f006cc627
Noemi <noemikow8@gmail.com>
2016-11-28 22:27:08 +0100

    set angular 2 structure and config files

---
+++ app/start-component/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'started-page',
+    templateUrl: `app/start-component/start-page.html`,
+    styleUrls: ['app/start-component/main.css'],
+    directives: []
+})
+export class AppComponent {}
