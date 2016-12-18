d1fc810946a318dac2db6556f6e69e7500d21907
beniamin1 <beniamingorzela@gmail.com>
2016-12-18 17:38:13 +0100

    Add change password

---
+++ backend/chagnePasswordWhenLogedIn.php
@@ -0,0 +1,33 @@
+<?php
+    require_once('User/user.php');
+    $user = new User();
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+        if(!isset($_POST["password1"]) || !isset($_POST["password2"]) || !isset($_POST["currentPassword"])){
+            $output = json_encode(array('error'=>true, 'message' => 'Podaj wszystkie dane'));
+    		die($output);
+        }else{
+            session_start();
+                if ($user->verifiToken() ==  true){
+                    if (strcmp($_POST["password1"], $_POST['password2']) === 0){
+                            if(strlen($password) <= 6){
+                                $password = md5(filter_var(trim($_POST["password1"]), FILTER_SANITIZE_STRING));
+                                $currentPasswodr = md5(filter_var(trim($_POST["currentPassword"]), FILTER_SANITIZE_STRING));
+ 
+                                $user->changePasswordLogin($password, $currentPasswodr, $_POST["currentPassword"]);
+                            }else{
+                                echo json_encode(array('error'=>true, 'message' => 'Haslo jest za krotkie'));
+                            }
+                    }else{
+                        echo json_encode(array('error'=>true, 'message' => 'Hasla nie sa takie same'));
+                    }
+                }else{
+                    echo "test";
+                }
+                
+        }
+	}
+
+   
+
+?>
