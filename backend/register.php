65cff7bd6f3e799e99e0702972df72c389b482ec
beniamin1 <beniamingorzela@gmail.com>
2016-12-18 17:26:54 +0100

    Fix register

--- backend/register.php
+++ backend/register.php
@@ -1,60 +1,68 @@
 <?php
     require_once('class/user.php');
-    $user = new User();
+    $user = new USER();
 
     
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    if(!isset($_POST["login"]) || !isset($_POST["mail"]) || !isset($_POST["password"]))
+    
+
+    if(!isset($_POST["login"]) || !isset($_POST["mail"]) || !isset($_POST["password"]) || !isset($_POST["password2"]))
         {
-        	$output = json_encode(array('error'=>true, 'maessage' => 'Input fields are empty!'));
+        	$output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
     		die($output);
     	}
+        
     	$uname = filter_var(trim($_POST["login"]), FILTER_SANITIZE_STRING);
     	$umail = filter_var(trim($_POST["mail"]), FILTER_SANITIZE_EMAIL);
-    	$password = filter_var(trim($_POST["password"]), FILTER_SANITIZE_EMAIL);
-
-    	if($uname=="")	{
-			echo  json_encode(array('error' => true, 'message' => "Podaj login"));
+    	$password = filter_var(trim($_POST["password"]), FILTER_SANITIZE_STRING);
+        $password2 = filter_var(trim($_POST["password2"]), FILTER_SANITIZE_STRING);
+        
+    	if((strcmp($uname, '') === 0))	{
+    		echo  json_encode(array('error' => true, 'message' => "Podaj login"));	
     	}
-    	else if($umail=="")	{
-    		echo  "Podaj mail";	
+    	else if (strcmp($umail, '') === 0)	{
+    		echo  json_encode(array('error' => true, 'message' => "Podaj mail"));	
     	}
     	else if(!filter_var($umail, FILTER_VALIDATE_EMAIL))	{
-			echo  json_encode(array('error' => true, 'message' => 'Podaj poprawny mail'));
+    	   echo  json_encode(array('error' => true, 'message' => 'Podaj poprawny mail'));
     	}
-    	else if($password=="")	{
-			echo  json_encode(array('error' => true, 'message' => "podaj haslo"));
+    	else if(strcmp($password, '') === 0)	{
+    		echo  json_encode(array('error' => true, 'message' => "podaj haslo"));
     	}
     	else if(strlen($password) < 6){
-			echo  json_encode(array('error' => true, 'message' => "Haslo musi być dluższe niż 6 liter"));	
-    	}
-    	else
+    		echo  json_encode(array('error' => true, 'message' => "Haslo musi być dluższe niż 6 liter"));	
+    	}else if(strcmp($password, $password2) !== 0){
+    		echo  json_encode(array('error' => true, 'message' => "Podane hasła nie są jednakowe"));
+		}else
     	{
     		try
     		{
     			$stmt = $user->runQuery("SELECT login, mail FROM Uzytkownicy WHERE login=:uname OR mail=:umail");
-                $stmt->bindparam(":uname", $uname);
+                $stmt->bindparam(":uname", $uname  );
     			$stmt->bindparam(":umail", $umail);
     			$stmt->execute();
     			$row=$stmt->fetch(PDO::FETCH_ASSOC);
                 
-    			if($row['login']==$uname) {
-					echo  json_encode(array('error' => true, 'message' => "Ten login jest zajety"));
+    			if(strcmp($uname, $row['login'])===0) {
+    				echo  json_encode(array('error' => true, 'message' => "Ten login jest zajety"));
     			}
-    			else if($row['mail']==$umail) {
-					echo  json_encode(array('error' => true, 'message' => "Ten mail jest zajety"));
+    			else if(strcmp($umail, $row['mail'])===0){
+    				echo  json_encode(array('error' => true, 'message' => "Ten mail jest zajety"));
     			}
     			else
     			{
-    				$user->register($uname,$umail,$upass);
+    				$user->register($uname,$umail,$password);
     			}
     		}
     		catch(PDOException $e)
     		{
-    			echo $e->getMessage();
+    			echo json_encode(array('error' => true, 'message' => $e->getMessage())); 
     		}
-    	}       
+    	}
+        
+        
     }
+
 ?>
 	
