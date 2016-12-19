b39d61339b974658d0e69d49d7a5cf68037e3cb0
beniamin1 <beniamingorzela@gmail.com>
2016-12-19 19:43:25 +0100

    Update UserInformation.php

--- backend/UserInformation.php
+++ backend/UserInformation.php
@@ -7,8 +7,13 @@
             $stmt = $user->runQuery("SELECT `id`,`login`,`mail`,`date_login`,`date_register`,`lat`,`lon`,`aktywne` FROM `Uzytkownicy`  where id=:id");
             $stmt->bindparam(":id",  $_SESSION['sesion_id']);
             $stmt->execute();
-            $reuslt=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            $reuslt=$stmt->fetch(PDO::FETCH_ASSOC);
             if($reuslt){
+                $stmth = $user->runQuery("select id, id_uzytkownik, image, name, date_create from Image where id_uzytkownik=:id");
+                $stmth->bindparam(":id",  $_SESSION['sesion_id']);
+                $stmth->execute();
+                $reuslth=$stmth->fetchAll(PDO::FETCH_ASSOC);
+                $reuslt['history'] = $reuslth;
                 echo  json_encode(array('error' => false, 'message' => $reuslt));
             }
         }
