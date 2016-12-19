9b4158f6a5972a19b71ff902e1900d6cb64ac95e
beniamin1 <beniamingorzela@gmail.com>
2016-12-19 09:17:27 +0100

    Add files via upload

--- backend/class/user.php
+++ backend/class/user.php
@@ -311,9 +311,21 @@ class User
 	
 	public function doLogout()
 	{
-		session_destroy();
-		unset($_SESSION['user_session']);
-        unset($_SESSION['sesion_id']);
+        session_start();
+        $_SESSION = array();
+        if (isset($_COOKIE[session_name()])) { 
+            setcookie(session_name(), '', time()-42000, '/'); 
+        }
+        
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+        }
+
+        session_destroy();
 		return true;
 	}
     
