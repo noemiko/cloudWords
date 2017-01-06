3583074d0032180ed64851e0145e6deedeb3f26c
beniamin1 <beniamingorzela@gmail.com>
2017-01-06 20:25:05 +0100

    Update saveImage.php

--- backend/saveImage.php
+++ backend/saveImage.php
@@ -5,15 +5,17 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if(!isset($_POST["image"])){
-        $output = json_encode(array('error'=>true, 'message' => 'Nie ma zdjęcia'));
+        $output = json_encode(array('error'=>true, 'message' => 'Brak danych'));
 		die($output);
 	}
     $name = "";
     if(isset($_POST['name'])){
         $name=$_POST['name'];
     }
+    
     $image = filter_var(trim($_POST["image"]), FILTER_SANITIZE_STRING);
     $name = filter_var(trim($_POST["name"]), FILTER_SANITIZE_STRING);
-    $user-> saveImage($image, $name);
+    $hash = bin2hex(mcrypt_create_iv(20, MCRYPT_DEV_URANDOM));
+    $user-> saveImage($image, $name, $hash);
 }
 ?>
