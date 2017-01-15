8ab0cbb2d6b365b2fcf4523967e9e73215bc9b20
beniamin1 <beniamingorzela@gmail.com>
2017-01-15 18:59:50 +0100

    Update user.php

--- backend/class/user.php
+++ backend/class/user.php
@@ -326,13 +326,13 @@ class User
        try{
             session_start();
             $_SESSION = array();
-            /*if (ini_get("session.use_cookies")) {
+            if (ini_get("session.use_cookies")) {
                 $params = session_get_cookie_params();
                 setcookie(session_name(), '', time() - 42000,
                 $params["path"], $params["domain"],
                 $params["secure"], $params["httponly"]
             );
-            }*/
+            }
             session_destroy();
     	    return true;
         }
