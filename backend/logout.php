5c48adb0fb89a95ae8b24e60d07290decc4286f4
beniamin1 <beniamingorzela@gmail.com>
2017-01-15 17:48:34 +0100

    Update logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -5,11 +5,15 @@
     $user = new User();
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        if($user -> is_loggedin()){
-            $user -> doLogout();
-	    //echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
-        }else{
-            //echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
+        try{
+       		if($user -> is_loggedin()){
+            		$user -> doLogout();
+    			echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
+        	}else{
+            		echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
+        	}
+        } catch (Exception $e) {
+             	echo  json_encode(array('error' => true, 'message' => $e->getMessage()));
         }
     }
 ?>
