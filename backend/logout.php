e67d7dd209a7598a0c50a10ec41526440c5cded4
beniamin1 <beniamingorzela@gmail.com>
2017-01-15 18:50:54 +0100

    Update logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -1,20 +1,17 @@
    <?php
-     try{
-    require_once('calss/user.php');
-    session_start();
-    session_regenerate_id(true);
-    $user = new User();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-        try{
-       		if($user -> is_loggedin()){
-            		$user -> doLogout();
-    			echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
-        	}else{
-            		echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
-        	}
-    }
-       } catch (Exception $e) {
-               echo  json_encode(array('error' => true, 'message' => $e->getMessage()));
+    try{
+        require_once('class/user.php');
+        $user = new USER();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            session_start();
+            if (isset($_SESSION['sesion_id'])){
+    		    $user -> doLogout();
+                echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
+            }else{
+                echo  json_encode(array('error' => false, 'message' => "Zostales wylogowany"));
+            }
         }
+    } catch (Exception $e) {
+        echo  json_encode(array('error' => true, 'message' => $e->getMessage()));
+    }
 ?>
